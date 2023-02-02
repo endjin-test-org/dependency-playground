c64a7316d6032a34cf1b7ccf130a078d418697f6
Charlotte <charlotte.gayton@gmail.com>
2023-02-02 11:06:46 +0000

    Updated scripted build

--- build.ps1
+++ build.ps1
@@ -71,7 +71,7 @@ param (
     [string] $BuildModulePath,
 
     [Parameter()]
-    [version] $BuildModuleVersion = "0.2.15",
+    [version] $BuildModuleVersion = "1.2.3",
 
     [Parameter()]
     [version] $InvokeBuildModuleVersion = "5.7.1"
